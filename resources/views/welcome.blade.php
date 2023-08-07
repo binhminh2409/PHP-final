c0c55f5399920701b2ee94ab0b25543e83be5427
BinhMinh <binhminh.vg@gmail.com>
2023-08-07 19:19:41 +0700

    Add existing project files prior to the push to GitHub.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+    <div class="col col-md-6 text-center">
+				<a href="{{ route('students.index') }}" class="btn btn-primary btn-sm">View All</a>
+	</div>
+@endsection('content')
